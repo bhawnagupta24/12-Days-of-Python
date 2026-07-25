5c7a52ba01d82b7d2b6d4c5ecffeb4d6f160d30d
Bhawna Gupta <bhawnasnbgupta77@gmail.com>
2026-07-25 23:42:40 +0530

    Day 1: Added Python basics and input/output programs

---
+++ Day-01/Basic python.py
@@ -0,0 +1,31 @@
+print("Hey i am bhawna & from today i am learning python")
+
+""" 
+hey this is work as multi line comment but it is docstring
+"""
+
+# sherni = "bhawna"
+
+age = 18
+
+SheryiansSchool = "students"  # pascal case
+
+sheryiansSchool = "students"  # camel case
+
+sheryians_school = "students"  # snake case
+a='A'
+print(ord(a))
+
+
+# Slicing
+
+a= 'BHAWNA'
+print(a[0:7:1])
+
+#Type conversion
+
+a = 12
+
+a = str(a)
+
+print(type(a))
---
+++ Day-01/InputOutput.py
@@ -0,0 +1,16 @@
+name = "akarsh"
+age = "23"
+
+print(name, age)
+
+
+print("hello my name is ", name, "and my age is",age)
+print(f"hello my name is  {name} and my age is {age}")
+
+#input
+
+name= str(input("What is ur name "))
+print(name)
+print(type(name))
+age = int(input("Enter age"))
+print(age)
