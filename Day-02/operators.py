57c21d2705cbc9fae1daa058c5233a7f12545c73
Bhawna Gupta <bhawnasnbgupta77@gmail.com>
2026-07-28 21:14:51 +0530

    Day 2: Practiced Python operators and logical expressions

---
+++ Day-02/operators.py
@@ -0,0 +1,42 @@
+a = 5
+b = 20
+
+print(a + b)
+print(b - a)
+print(a * b)
+
+print(b / a)
+print(b // a)
+
+
+# compound assignment operations
+
+a = 20
+
+a += 20
+
+a += 40
+
+print(a)
+
+a= 12.1
+b=12
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(23>=23)
+
+print(ord('A'))
+
+print(ord("A"))
+print(ord("B"))
+
+print("ABC" > "ACD")
+
+
+# LOGICAL OPERATORS
+
+print(123 > 100 and 34 == 34 and 45 < 90 and 12 > 20)
+print(12 != 12 or 23 == 45 or 67 == 56 or 10 > 5)
+print(not 12==12)
